98c45995d159d50523926fef21809159952d0911
SanjanaSunkara1 <isunkarasanjana@gmail.com>
2025-08-15 14:12:02 -0500

    created new frmWeatherAPI

--- frmWeatherAPI.cs
+++ frmWeatherAPI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WeatherAPI
 {
-    public partial class Form1 : Form
+    public partial class frmWeatherAPI : Form
     {
-        public Form1()
+        public frmWeatherAPI()
         {
             InitializeComponent();
         }
