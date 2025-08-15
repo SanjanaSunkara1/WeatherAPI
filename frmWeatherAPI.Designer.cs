9a57fb4c5619ba55014dc549f05f59a9fcb3ecf6
SanjanaSunkara1 <isunkarasanjana@gmail.com>
2025-08-15 14:27:26 -0500

    2 buttons

--- frmWeatherAPI.Designer.cs
+++ frmWeatherAPI.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.button1.Location = new System.Drawing.Point(23, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Weather (XML)";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.button2.Location = new System.Drawing.Point(295, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Get Weather (JSON)";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // frmWeatherAPI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(546, 536);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmWeatherAPI";
             this.Text = "frmWeatherAPI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- frmWeatherAPI.cs
+++ frmWeatherAPI.cs
@@ -16,5 +16,7 @@ namespace WeatherAPI
         {
             InitializeComponent();
         }
+
+        
     }
 }
